37d0edb8a600520db6cffd4c32d997970edeb013
dbswp <dbswp980427@gmail.com>
2023-03-25 23:08:31 +0900

    react 함수형과 클래스형 컴포넌트 연습

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class ClassComponent extends Component {
-  render() {
-    return <h1>Class Component 입니다.!</h1>;
-  }
-}
+// class ClassComponent extends Component {
+//   render() {
+//     return <h1>Class Component 입니다.!</h1>;
+//   }
+// }
 
-export default ClassComponent;
+// export default ClassComponent;
 
-// const Functional = () => {
-//   return <h1>Functional Component 입니다.!</h1>;
-// };
+const Functional = () => {
+  return <h1>Functional Component 입니다.!</h1>;
+};
 
-// export default Functional;
+export default Functional;
