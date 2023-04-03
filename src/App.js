983a8a7b960ea912d14517a095db0e7558b19f13
dbswp <dbswp980427@gmail.com>
2023-04-03 10:19:50 +0900

    scss사용해서 style적용

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import styled, { keyframes } from 'styled-components';
-
-const AppDiv = styled.div`
-  text-align: center;
-`;
-const LogoSwitch = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-  `;
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-  animation: ${LogoSwitch} infinite 20s linear;
-`;
-const AppA = styled.a`
-  color: #61dafb;
-`;
+import './App.css';
+import TestScss from './components/TestScss';
 
 function App() {
   return (
-    <AppDiv>
-      <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.!!!!!!!
-        </p>
-        <AppA
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppA>
-      </AppHeader>
-    </AppDiv>
+    <div className="App">
+      <TestScss />
+    </div>
   );
 }
 
---
+++ src/components/TestScss.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/TestScss.scss';
+
+// scss 사용
+export default function TestScss() {
+  return (
+    <div className="component-root">
+      <h1> CSS 테스트 컴포넌트 입니다.</h1>
+      <span>해당 컴포넌트를 기본 CSS로 꾸며 보자</span>
+    </div>
+  );
+}
